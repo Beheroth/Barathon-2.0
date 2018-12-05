983e0b57c8b72f5a482f3588748a7bda95e90420
Beheroth <arnaud.peeters95@gmail.com>
2018-12-05 12:08:08 +0100

    Update StrategyBars.java
    Reduced useless parameters required to solve

--- Application/src/main/java/StrategyBars.java
+++ Application/src/main/java/StrategyBars.java
@@ -7,12 +7,10 @@ public class StrategyBars implements Strategy {
   /**
    *  Method solve.
    *
-   * @param p position of the user.
-   * @param c carcateristics of the user.
+   * @param u user to search from.
    * @return list of places.
    */
-    public final ArrayList<Place> solve(final Position p,
-    final Caracteristics c) {
+    public final ArrayList<Place> solve(final User u) {
         ArrayList<Place> lst = new ArrayList<Place>();
         // Takes the parameter position and returns all the bars that in 100km.
         // around the position.
