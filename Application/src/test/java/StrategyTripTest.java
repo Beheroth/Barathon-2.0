600e6a0fc5aee488077229786e1331736f1cf275
Sam BERTRAND <15027@ecam.be>
2018-12-12 11:32:26 +0100

    fixing symbol error 3

--- Application/src/test/java/StrategyTripTest.java
+++ Application/src/test/java/StrategyTripTest.java
@@ -2,19 +2,30 @@ import org.junit.*;
 
 public class StrategyTripTest {
 
-	public Position p = new Position(16.38, 48.25);
-	public Adress a = new Address("Rue du barathon", "10", p );
-	public Menu m = new Menu();
-	public Caracteristics c = new Caracteristics();
+	public Position p;
+	public Adress a ;
+	public Menu m ;
+	public Caracteristics c ;		
+	public Place pl ;
+	public ArrayList<Place> ar;
+	public Drink d;
+
+	@before
+	public void InitClasses() {
+		p = new Position(16.38, 48.25);
+		a = new Address("Rue du barathon", "10", p );
+		m = new Menu();
+		c = new Caracteristics();
 		c.set(false, false, false, false, false, false, false, false);
-	public Place pl = new Place(1, "Bar quelconque", a, m, c);
-	public List<Place> ar = new ArrayList<>(pl);
-	public Drink d = new Drink("Boisson soft", 2, 0, true);
+		pl = new Place(1, "Bar quelconque", a, m, c);
+		ar = new ArrayList<pl>;
+		d = new Drink("Boisson soft", 2, 0, true);
+	}
 
 	@Test
 	public void TestFilter() {        
 		filter(ar, c);
-        Assert.assertTrue(ar.isEqual(new arraylist<>(pl)));
-        Assert.assertFalse(ar.isEqual(new arraylist<>()));
+        Assert.assertTrue(ar.isEqual(new ArrayList<>(pl)));
+        Assert.assertFalse(ar.isEqual(new ArrayList<>()));
 	}
 }
