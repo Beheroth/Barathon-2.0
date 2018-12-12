ff434047467181381f0c2e7402f4b6e2d6125ad0
Beheroth <arnaud.peeters95@gmail.com>
2018-12-12 09:22:35 +0100

    Updated methods and deleted circularsort

--- Application/src/main/java/StrategyTrip.java
+++ Application/src/main/java/StrategyTrip.java
@@ -5,51 +5,35 @@ import java.util.ArrayList;
  * Class to find the trip of bars.
  */
 public class StrategyTrip implements Strategy {
-  /**
-   *  Method solve.
-   *
-   * @param u user to search from.
-   * @return list of ordered places.
-   */
+    /**
+     * Method solve.
+     *
+     * @param u user to search from.
+     * @return list of ordered places.
+     */
     public final ArrayList<Place> solve(final User u) {
         Caracteristics c = u.getCurrentCaracteristics();
         Position pos = u.getPosition();
         ArrayList<Place> trip = new ArrayList<Place>();
-        //trip = DBAccess.getNearbyPlaces(p);
+        //trip = DBAccess.getNearbyPlaces(u);
         filter(trip, c);
-        circularSort(trip, pos);
+        sort(trip, pos);
 
         // Take parameters position and carcateristics which match the places.
         return trip;
     }
 
-    private void filter(ArrayList<Place> trip, Caracteristics userCaracteristics){
+    private void filter(ArrayList<Place> trip, Caracteristics userCaracteristics) {
         for (Place place : trip) {
             Caracteristics placeCaracteristics = place.getCaracteristics();
-            if(!userCaracteristics.equals(placeCaracteristics)){
+            if (!userCaracteristics.equals(placeCaracteristics)) {
                 trip.remove(place);
             }
         }
     }
 
-    private void circularSort(ArrayList<Place> trip, Position pos){
-        Place firstPlace = getNearest(trip, pos);
-    }
+    private void sort(ArrayList<Place> trip, Position pos) {
+        ArrayList<Place> sortedtrip = new ArrayList<Place>();
 
-    private Place getNearest(ArrayList<Place> places, Position pos){
-        Place result = null;
-        try{
-            result = places.get(0);   //What if the list is empty
-        }
-        catch (Exception e){
-            System.out.println("Couldn't get nearest place");
-        }
-
-        for(Place place : places){
-            if(pos.getDistanceFrom(place.getAddress().getPosition()) < pos.getDistanceFrom(result.getAddress().getPosition())){
-                result = place;
-            }
-        }
-        return result;
     }
 }
