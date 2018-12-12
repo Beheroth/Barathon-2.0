6fdb829252ba0c7e6f327137ebb6074016b86dc2
Sam BERTRAND <15027@ecam.be>
2018-12-12 11:53:53 +0100

    symbol error fix 7

--- Application/src/test/java/StrategyTripTest.java
+++ Application/src/test/java/StrategyTripTest.java
@@ -2,14 +2,23 @@ import org.junit.*;
 
 public class StrategyTripTest {
 
-    public void InitClasses() {
+	public Position p;
+	public Address a ;
+	public Menu m ;
+	public Caracteristics c ;		
+	public Place pl ;
+	public ArrayList<Place> ar;
+	public Drink d;
+
+	@Before
+
+	public void InitClasses() {
 		p = new Position(16.38, 48.25);
 		a = new Address("Rue du barathon", "10", p );
 		m = new Menu();
 		c = new Caracteristics();
 		c.set(false, false, false, false, false, false, false, false);
 		pl = new Place(1, "Bar quelconque", a, m, c);
-		list<Place> ar = new arraylist<>(pl);
 		d = new Drink("Boisson soft", 2, 0, true);
 	}
 
