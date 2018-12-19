2ba9e57b05266e81b448291073519f8b8183f1a3
Sam BERTRAND <15027@ecam.be>
2018-12-19 12:02:35 +0100

    test solve done

--- Application/src/test/java/StrategyBarTest.java
+++ Application/src/test/java/StrategyBarTest.java
@@ -4,27 +4,39 @@ import org.junit.*;
 public class StrategyBarTest {
 
     public Position p;
-    public Address a ;
-    public Menu m ;
+    //public Address a ;
+    //public Menu m ;
     public Caracteristics c ;
-    public Place pl ;
-    public ArrayList<Place> ar;
-    public Drink d;
+    //public Place pl ;
+    public ArrayList<Place> result;
+    //public Drink d;
     public StrategyBars sb;
-
+    public User u;
+    public Preferences pr;
 
     @Before
     public void InitClasses() {
         p = new Position(16.38, 48.25);
-        a = new Address("Rue du barathon", "10", p );
-        m = new Menu();
+        //a = new Address("Rue du barathon", "10", p );
+        //m = new Menu();
         c = new Caracteristics();
         c.set(false, false, false, false, false, false, false, false);
-        pl = new Place(1, "Bar quelconque", a, m, c);
-        ar = new ArrayList<Place>();
-        ar.add(pl);
-        d = new Drink("Boisson soft", 2, 0, true);
+        //pl = new Place(1, "Bar quelconque", a, m, c);
+        //ar = new ArrayList<Place>();
+        //ar.add(pl);
+        //d = new Drink("Boisson soft", 2, 0, true);
         sb = new StrategyBars();
+        pr = new Preferences();
+        pr.addCaracteristics(c);
+        u = new User("fraonçois", p, pr);
     }
 
+
+    @Test
+    public void TestSolve() {
+        sb.solve(u);
+        result =  u.getTrips();
+        Assert.assertTrue(result.isEmpty());
+        Assert.assertFalse(result.isEmpty());
+    }
 }
